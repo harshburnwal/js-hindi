afe3aa0a5d5fd7d7af8a794c8362092b365f3b77
Harsh <123619699+harshburnwal@users.noreply.github.com>
2024-01-27 10:36:41 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 726378
+let accountEmail = "harsh@cypher.com"
+var accountPassword = "98765"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hb@hb.com"
+accountPassword = "873829"
+accountCity = "Gurgaon"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
