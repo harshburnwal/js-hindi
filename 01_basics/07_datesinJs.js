5b8ab1be4368c50a766358f07a93b15a8ef9e949
Harsh <123619699+harshburnwal@users.noreply.github.com>
2024-01-28 12:15:15 +0530

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -9,7 +9,7 @@ let myDate = new Date()
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+let myCreatedDate = new Date("01-28-2024")
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
